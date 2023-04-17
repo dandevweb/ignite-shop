64d73e3f6c6fc7e3c03f0e204727a38469cdf9e2
dandevweb <dandevweb@gmail.com>
2023-04-17 20:28:05 -0300

    seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { HomeContainer, Product } from "@/styles/pages/home"
+import Head from 'next/head'
 import { useKeenSlider } from 'keen-slider/react'
 import Image from "next/image"
 
@@ -25,21 +26,26 @@ export default function Home({ products }: HomeProps) {
   })
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
+    <>
+      <Head>
+        <title>Home - Ignite Shop</title>
+      </Head>
+      <HomeContainer ref={sliderRef} className="keen-slider">
 
-      {products.map(product => {
-        return (
-          <Product href={`product/${product.id}`} key={product.id} className="keen-slider__slide" prefetch={false}>
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
-        )
-      })}
+        {products.map(product => {
+          return (
+            <Product href={`product/${product.id}`} key={product.id} className="keen-slider__slide" prefetch={false}>
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          )
+        })}
 
-    </HomeContainer>
+      </HomeContainer>
+    </>
   )
 }
 
